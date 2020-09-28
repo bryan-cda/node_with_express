1b4580d07614d9c8eb72abdaf349266c5d7521ef
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2020-09-28 20:53:18 -0300

    Initial commit.

---
+++ functions/calc.js
@@ -0,0 +1,17 @@
+var sumFunction = require("./sum")
+
+var multFunction = require("./mult")
+
+var divFunction = require("./div")
+
+var subFunction = require("./sub")
+
+console.log(sumFunction(10,2))
+
+console.log(multFunction(10,2))
+
+console.log(divFunction(10,2))
+
+console.log(subFunction(10,2))
+
+
---
+++ functions/sub.js
@@ -0,0 +1,7 @@
+const sum = require("./sum");
+
+function sub(a, b){
+    return a-b;
+}
+
+module.exports = sub;
