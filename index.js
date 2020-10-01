6771e5be7c59fe5ae6592f6757db71f05d5c06df
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2020-10-01 19:58:17 -0300

    Create dynamic routes using parameters.

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require("express")
 const app = express()
 
-var port = 8081
-
-app.get("/app-node", function(req, res){
-    res.send('Welcome to my app')
+var port = 8082
+app.get("/app-node/:user/:password", function(req, res){
+    res.send(req.params);
 });
 
 app.get("/about", function(req, res){
-    res.send("We are a software builder.")
+    res.send("We are a software builder.");
 });
 
 app.get("/contact", function(req, res){
-    res.send("WhatsApp: (21) 999091-7198")
+    res.send("WhatsApp: (21) 999091-7198");
 });
 
 function message(msg){
-    console.log(msg)
+    console.log(msg);
 }
 app.listen(port, message(
     'Server is open listening on port: ' + port
