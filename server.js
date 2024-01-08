9541a1196af2ca35da30abf0cc2d27f8d22eb062
bryanduartecd2 <140202022+bryanduartecd2@users.noreply.github.com>
2024-01-08 17:40:25 -0300

    Crate new routes.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import http from "http";
 
+const route = {
+    "/":"Node with Express presentation",
+    "/foo": "foo",
+    "/bar": "bar"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plan"});
-    res.end("Node.js with Express")
+    res.end(route[req.url])
 });
 
 server.listen(3000, () => {
