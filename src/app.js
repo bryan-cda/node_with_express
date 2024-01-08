3cb12b5c4c670108ac95c7a603155fae6aa9f151
bryan-cda <albuquerque.bry.n@gmail.com>
2024-01-08 19:18:07 -0300

    Create a find books in memory endpoint with express.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
 const app = express();
 
+const books = [
+    {
+        id: 1,
+        title: "foo"
+    },
+    {
+        id: 2,
+        title: "bar"
+    }
+];
+
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Express");
+});
+
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
 });
 
 export default app;
