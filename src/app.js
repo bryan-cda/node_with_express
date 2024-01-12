e42ea5b110168b197af8e663827edaaaeced8340
bryan-cda <albuquerque.bry.n@gmail.com>
2024-01-12 01:08:58 -0300

    Create find book by id, delete book and update book endpoint.

--- src/app.js
+++ src/app.js
@@ -5,16 +5,13 @@ const app = express();
 app.use(express.json());
 
 
-const books = [
-    {
-        id: 1,
-        title: "foo"
-    },
-    {
-        id: 2,
-        title: "bar"
-    }
-];
+const books = [{id: 1, title: "foo"}, {id: 2, title: "bar"}];
+
+function findBookById(id){
+    return books.findIndex(book => {
+        return book.id === Number(id);
+    })
+};
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Express");
@@ -27,6 +24,24 @@ app.get("/books", (req, res) => {
 app.post("/books", (req, res) =>{
     books.push(req.body);
     res.status(201).send("book has been created.")
+});
+
+app.get("/books/:id", (req, res) => {
+    const index =  findBookById(req.params.id);
+    res.status(200).json(books[index]);
+});
+
+app.put("/books/:id", (req, res) => {
+    const index =  findBookById(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+    const index = findBookById(req.params.id);
+    books.splice(index, 1);
+    res.status(204).send("Book deleted.")
 })
+
 
 export default app;
