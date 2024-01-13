fd226e9eec3926fb308416081f1928199d85b5a2
bryan-cda <albuquerque.bry.n@gmail.com>
2024-01-13 10:53:37 -0300

    Create delete book endpoint and route.

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,33 +1,31 @@
 import book from "../model/book.js";
 
 class bookController {
-  
     static async listBooks(req, res) {
         try {
             const books = await book.find({});
             res.status(200).json(books);
         } catch(error){
-            res.status(500).json(`${error} - error on request`)
+            res.status(500).json(`${error} - error on execute request to find all books`);
         }   
     };
 
     static async addBook(req, res) {
         try{
             const newBook = await book.create(req.body);
-            res.status(200).json({message: "Book created", book: newBook})
-
+            res.status(201).json({message: "successfully created", book: newBook});
         } catch (error){
-            res.status(500).json({message: `${error.message} - error when add book: ${req.body}`})
+            res.status(500).json({message: `${error.message} - error when attempt to add book: ${req.body}`});
         }
     };
 
     static async findBookById(req, res) {
         try {
             const id = req.params.id;
-            const foundBook = await book.findById(id);
-            res.status(200).json(foundBook);
+            const bookFound = await book.findById(id);
+            res.status(200).json(`update execute for book: ${bookFound}`);
         } catch(error){
-            res.status(500).json(`${error.message} - error on search book by id: ${req.body.id}`)
+            res.status(500).json(`${error.message} - error on attempt to search book by id: ${req.params.id}`)
         }
     };
 
@@ -37,7 +35,17 @@ class bookController {
             await book.findByIdAndUpdate(id, req.body);
             res.status(200).json(updatedBook);
         } catch(error){
-            res.status(500).json(`${error.message} - error on update book: ${req.body}`)
+            res.status(500).json(`${error.message} - error on attempt to update book: ${req.body}`)
+        }
+    };
+
+    static async deleteBook(req, res){
+        try {
+            const id = req.params.id;
+            await book.findByIdAndDelete(id);
+            res.status(200).send("book has been deleted")
+        } catch (error){
+            res.status(500).json(`${error.message} - error on attempt to delete book by id: ${req.params.id}`)
         }
     }
 }
